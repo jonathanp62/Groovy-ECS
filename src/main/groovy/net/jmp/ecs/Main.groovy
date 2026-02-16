c9db7480b61180f8c19ddf0f31852db6059b6da9
Jonathan Parker <jonathanp62@gmail.com>
2026-02-16 07:17:21 -0500

    Implemented setting the ‘Implementation-Version’ in the jar and fetching it from the Main class

--- src/main/groovy/net/jmp/ecs/Main.groovy
+++ src/main/groovy/net/jmp/ecs/Main.groovy
@@ -1,5 +1,46 @@
 package net.jmp.ecs
 
+/*
+ * (#)Main.groovy   1.0.0   02/16/2026
+ *
+ * @author    Jonathan Parker
+ * @version   1.0.0
+ * @since     1.0.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025, 2026 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * The main class for the ECS application
+ */
 static void main(String[] args) {
-    println "Hello world!"
+    /* Getting the version from the build.gradle file only works for jar deployments */
+
+    def version = getClass().package.implementationVersion
+
+    if (version == null) {
+        println "Hello ECS!"
+    } else {
+        println "Hello ECS, version $version!"
+    }
 }
